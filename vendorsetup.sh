310673a0f57a64ddd8475d7696a02e34839d1ee7
Justin Rogers <justin.a.rogers@gmail.com>
2013-10-06 21:05:36 -0500

    Jflte love

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,10 @@
 add_lunch_combo tg_jflteatt-userdebug
+add_lunch_combo tg_jfltecsp-userdebug
+add_lunch_combo tg_jfltecri-userdebug
+add_lunch_combo tg_jflteusc-userdebug
+add_lunch_combo tg_jfltecan-userdebug
+add_lunch_combo tg_jfltespr-userdebug
+add_lunch_combo tg_jfltevzw-userdebug
+add_lunch_combo tg_jfltetmo-userdebug
 add_lunch_combo tg_mako-userdebug
 add_lunch_combo tg_m7ul-userdebug
