95f86ecddd6a46e730dac3d47442c86f84daf509
Matt Mower <mowerm@gmail.com>
2013-10-29 16:48:24 -0500

    Let common_mini_phone inherit from commonized telephony.mk
    See: http://review.cyanogenmod.org/52650
    
    Change-Id: Ifb074fb380cdd84e6ace49631ec54a8cef45c457
    
    Conflicts:
    	config/common_mini_phone.mk

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -14,15 +14,5 @@ ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/Gummy/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
 endif
-# World APN list
-PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
-
+$(call inherit-product, vendor/Gummy/config/telephony.mk)
