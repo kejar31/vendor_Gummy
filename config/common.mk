23a8b814898351cecb5e61b7ed3a22231b2749fc
cphelps76 <cphelps76@gmail.com>
2013-11-04 00:15:40 -0600

    build InvertedCalendar when hostname check reads my hostname removed inverted calendar overlay"

--- config/common.mk
+++ config/common.mk
@@ -9,6 +9,7 @@ SUPERUSER_PACKAGE_PREFIX := com.android.settings.gummy.superuser
 HOST_CHECK := $(shell hostname)
 ifeq ($(HOST_CHECK), cphelps76-HP-Pavilion-dv7)
     PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/inverted
+    PRODUCT_PACKAGES += InvertedCalendar
 else
     PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/common
 endif
