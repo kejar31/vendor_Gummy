CyanogenMod/android_vendor_cm

Add Theme Engine to the builds
Change-Id: I57568ebef3f9800e0ad7c39b6352186470ad9f08

config/themes_common.mk
@@ -1,0 +1,8 @@
+# T-Mobile theme engine
+PRODUCT_PACKAGES += \
+       ThemeManager \
+       ThemeChooser \
+       com.tmobile.themes
+
+PRODUCT_COPY_FILES += \
+       vendor/cm/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml

kejar31/vendor_Gummy

config/themes_common.mk
@@ -1,0 +1,8 @@
+# T-Mobile theme engine
+PRODUCT_PACKAGES += \
+       ThemeManager \
+       ThemeChooser \
+       com.tmobile.themes
+
+PRODUCT_COPY_FILES += \
+       vendor/Gummy/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml