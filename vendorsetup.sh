86c0f964024056b12d5806cf142a308b602e6e9a
papi92 <papi92@droid-hive.com>
2013-12-10 15:34:43 -0500

    Add in hammerhead for nightly status
    Change-Id: I4d185d2be2f290c073565cf22063a1c99b8edbd5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,7 @@ add_lunch_combo tg_e980-userdebug
 add_lunch_combo tg_encore-userdebug
 add_lunch_combo tg_flo-userdebug
 add_lunch_combo tg_grouper-userdebug
+add_lunch_combo tg_hammerhead-userdebug
 add_lunch_combo tg_jflteatt-userdebug
 add_lunch_combo tg_jfltecsp-userdebug
 add_lunch_combo tg_jfltecri-userdebug
