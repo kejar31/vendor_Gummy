a28989ddf278a6fc53d9cfc4094abd0ddfbeed06
Abhisek Devkota <ciwrl@cyanogenmod.com>
2013-12-23 22:55:26 -0800

    Remove unused audio mk files
    These are empty and useless.
    
    Change-Id: I23cd1be105f7bddbf555a35e10efcc92a7c1eb47
    
    Conflicts:
    	config/common_full.mk

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,9 +1,6 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/Gummy/config/common.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
 # Include CM audio files
 include vendor/Gummy/config/tg_audio.mk
 
