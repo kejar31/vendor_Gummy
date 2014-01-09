4f1c8653ba7aedcf4d6307a6fe69d8f5e3accd23
papi <papi92@droid-hive.com>
2014-01-09 11:22:38 -0500

    add in T-Mobile G2 variant
    Change-Id: I1f5b288b229b3525feae7f633b0fad6aca964b29

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,6 @@
 add_lunch_combo tg_d800-userdebug
 add_lunch_combo tg_d802-userdebug
+add_lunch_combo tg_d802-userdebug
 add_lunch_combo tg_d2att-userdebug
 add_lunch_combo tg_d2cri-userdebug
 add_lunch_combo tg_d2mtr-userdebug
