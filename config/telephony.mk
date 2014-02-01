namdeptrai2003/android_vendor_dnd

cm: add selective based spn (2/2)
* patch set 1 to 7 doesn't fix the problem, it bruce forces the spn
list to ALL DEVICES.
* this change and http://review.cyanogenmod.org/#/c/57393/
will attack the problem at lowest level, customized it for speed
and memory.
*  the best part is that it handles letters and exit the
function when it find a letter in the response from ril.

Change-Id: Ia63309acf38617e68b98ff1068463a942fa793df

config/telephony.mk
@@ -6,6 +6,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
+# Selective SPN list for operator number who has the problem. 
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     Mms \

kejar31/vendor_Gummy

Conflicts:
	config/telephony.mk

config/telephony.mk
@@ -6,7 +6,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/Gummy/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
-# SIM Toolkit
+# Selective SPN list for operator number who has the problem. 
+PRODUCT_COPY_FILES += \
+    vendor/Gummy/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+
+
+# Telephony packages
 PRODUCT_PACKAGES += \
     Mms \
     Stk \