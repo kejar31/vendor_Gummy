namdeptrai2003/android_vendor_dnd

Exclude extra LatinIME dictionaries in "mini" builds
Change-Id: I935f1ba5addcff2b2e4d738550db3ca6916fe07f

config/common_full.mk
@@ -7,6 +7,9 @@ $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
+# Include CM LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+
 # Optional CM packages
 PRODUCT_PACKAGES += \
     Galaxy4 \

kejar31/vendor_Gummy

Conflicts:
	config/common.mk
	config/common_full.mk

config/common_full.mk
@@ -7,6 +7,9 @@ $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 # Include Gummy audio files
 include vendor/Gummy/config/tg_audio.mk
 
+# Include Gummy LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/Gummy/overlay/dictionaries
+
 # Optional Gummy packages
 PRODUCT_PACKAGES += \
     Galaxy4 \