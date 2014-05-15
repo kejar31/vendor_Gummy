f487840aa9c109bc988855bb968c05e2cab1baa9
papi92@Gummy <papi92@droid-hive.com>
2014-05-15 02:37:33 +0200

    add in m7spr
    Change-Id: I698b7ef1728f09a291a391b2198e2e3e10d5a547

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,7 @@ add_lunch_combo tg_jflte-userdebug
 add_lunch_combo tg_maguro-userdebug
 add_lunch_combo tg_mako-userdebug
 add_lunch_combo tg_m7-userdebug
+add_lunch_combo tg_m7spr-userdebug
 add_lunch_combo tg_m7vzw-userdebug
 add_lunch_combo tg_m8-userdebug
 add_lunch_combo tg_maserati-userdebug
